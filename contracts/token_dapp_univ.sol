36224fb1de3e9edb394f37998b66d17aefbe23a5
rkubin <richard@phyla.ai>
2021-03-05 18:08:05 -0500

    init commit

---
+++ contracts/token_dapp_univ.sol
@@ -0,0 +1,23 @@
+
+pragma solidity 0.5.16;
+
+import "@openzeppelin/contracts/token/ERC20/StandardToken.sol";
+import "@openzeppelin/contracts/token/ERC20/DetailedERC20.sol";
+
+
+contract SimpleToken is DetailedERC20, StandardToken {
+  constructor(
+    string _name,
+    string _symbol,
+    uint8 _decimals,
+    uint256 _amount
+  )
+    DetailedERC20(_name, _symbol, _decimals)
+    public
+  {
+    require(_amount > 0, "amount has to be greater than 0");
+    totalSupply_ = _amount.mul(10 ** uint256(_decimals));
+    balances[msg.sender] = totalSupply_;
+    emit Transfer(address(0), msg.sender, totalSupply_);
+  }
+}
